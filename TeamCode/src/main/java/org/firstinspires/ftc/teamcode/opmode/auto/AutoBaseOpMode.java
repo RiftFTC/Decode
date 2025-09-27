bd861aaf18af5e483c490059c8d715f705e0811c
DevMello <kol.kid.py@gmail.com>
2025-09-27 13:48:50 -0700

    created an auto template

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoBaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/AutoBaseOpMode.java
@@ -2,27 +2,37 @@ package org.firstinspires.ftc.teamcode.opmode.auto;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.acmerobotics.roadrunner.Pose2d;
 import com.arcrobotics.ftclib.command.Command;
-import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.command.Subsystem;
 import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.subsystems.*;
+import org.firstinspires.ftc.teamcode.util.Robot;
 import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
-import org.firstinspires.ftc.teamcode.subsystems.DriveSys;
-import org.firstinspires.ftc.teamcode.subsystems.TurretSys;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
 
 public class AutoBaseOpMode extends OpMode {
 
-    MecanumDrive drive;
-    protected MotorEx fl, fr, bl, br, shooter;
-    protected SimpleServo turret, pitch;
+    protected MotorEx fl, fr, bl, br, shooter, intake;
+    protected SimpleServo turret, pitch, left, middle, right;
+    protected RevColorSensorV3 leftC, middleC, rightC;
     protected DriveSys driveSys;
     protected TurretSys turretSys;
+    protected IntakeSys intakeSys;
+    protected SorterSys sorterSys;
+    protected OrchestratorSys orchestratorSys;
+
+
+    private AprilTagProcessor aprilTag;
+    private VisionPortal visionPortal;
 
     @Override
     public void init() {
@@ -31,6 +41,19 @@ public class AutoBaseOpMode extends OpMode {
         configHw();
         initSys();
         initMisc();
+
+    }
+
+    @Override
+    public void init_loop() {
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        if (!currentDetections.isEmpty()) {
+            for (AprilTagDetection tag : currentDetections) {
+                Robot.motif = SorterSys.getMotifFromAprilTag(tag.id);
+                tad("Tag ID", tag.id);
+                tad("Motif", Robot.motif);
+            }
+        }
     }
 
     public void initHw() {
@@ -39,8 +62,15 @@ public class AutoBaseOpMode extends OpMode {
         bl = new MotorEx(hardwareMap, "bl");
         br = new MotorEx(hardwareMap, "br");
         shooter = new MotorEx(hardwareMap, "shooter");
+        intake = new MotorEx(hardwareMap, "intake");
         turret = new SimpleServo(hardwareMap, "turret", 0, 360);
         pitch = new SimpleServo(hardwareMap, "pitch", 0, 360);
+        left = new SimpleServo(hardwareMap, "left", 0, 360);
+        middle = new SimpleServo(hardwareMap, "middle", 0, 360);
+        right = new SimpleServo(hardwareMap, "right", 0, 360);
+        leftC = hardwareMap.get(RevColorSensorV3.class, "leftC");
+        middleC = hardwareMap.get(RevColorSensorV3.class, "middleC");
+        rightC = hardwareMap.get(RevColorSensorV3.class, "rightC");
     }
 
     public void configHw() {
@@ -48,11 +78,19 @@ public class AutoBaseOpMode extends OpMode {
     }
 
     public void initSys() {
+        intakeSys = new IntakeSys(intake);
+        sorterSys = new SorterSys(left, middle, right, leftC, middleC, rightC);
     }
 
     public void initMisc() {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        aprilTag = new AprilTagProcessor.Builder()
+                .build();
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(webcamName);
+        builder.addProcessor(aprilTag);
+        visionPortal = builder.build();
     }
 
     protected void tad(String caption, Object value) {
@@ -76,6 +114,6 @@ public class AutoBaseOpMode extends OpMode {
     @Override
     public void stop() {
         super.stop();
-        Robot.startPose = drive.localizer.getPose();
+        Robot.startPose = driveSys.drive.localizer.getPose();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/FarRed.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.opmode.auto;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.DriveSys;
+import org.firstinspires.ftc.teamcode.subsystems.OrchestratorSys;
+import org.firstinspires.ftc.teamcode.subsystems.TurretSys;
+import org.firstinspires.ftc.teamcode.util.ActionCommand;
+import org.firstinspires.ftc.teamcode.util.Robot;
+
+@Autonomous(name = "Far Red", group = "Auto")
+public class FarRed extends AutoBaseOpMode{
+
+    Action leave;
+
+    @Override
+    public void init() {
+        super.init();
+        Robot.startPose = new Pose2d(0,0, Math.toRadians(90));
+        driveSys = new DriveSys(hardwareMap);
+        turretSys = new TurretSys(turret, pitch, shooter, driveSys.voyager);
+        orchestratorSys = new OrchestratorSys(intakeSys, sorterSys, turretSys);
+        register(driveSys, turretSys, intakeSys, sorterSys, orchestratorSys);
+        telemetry.addData("Auto", "Far Red");
+        telemetry.update();
+
+        leave = driveSys.drive.actionBuilder(new Pose2d(0,0, Math.toRadians(90)))
+                .lineToY(-10)
+                .build();
+
+        schedule(
+                new SequentialCommandGroup(
+                        new ActionCommand(leave)
+                )
+        );
+    }
+}
